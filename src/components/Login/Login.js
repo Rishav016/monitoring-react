244f6061488f546efa99eb47ecf085578ad72793
Rishav Mahajan <mrishav016@gmail.com>
2022-04-19 10:06:35 +0530

    Login component and initalization of react app

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <h1>welcome to monitoring-react!</h1>
+      <h3>Lets build a great clone of outdated monitoring </h3>
+    </div>
+  );
+}
+
+export default Login;
